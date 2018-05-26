e930bed1ab426f3556b50eeb6df17d712fb8262a
Guido Ruiz <guidoruiz4@gmail.com>
2018-05-25 21:52:04 -0400

    fixed it

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,12 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router()
 var moment = require('moment')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(moment('5/25/2018').fromNow())
+  res.json({
+    text: moment('5/25/2018').fromNow()
+  })
 });
 
-module.exports = router;
+module.exports = router
